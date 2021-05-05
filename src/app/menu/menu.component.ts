34d3d6d06ad84f2c41db05c2ad1882e154e0487b
maxwellfundi <fundimaxwell@gmail.com>
2021-05-05 10:03:06 +0300

    adding contact us section

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -76,6 +76,7 @@ const MENU_ITEMS: MenuItem[] = [
       { name: "Parents Survey", href: "/contactus",  fragment: "parents-survey" },
      /* { name: "Champions of Children Survey", href: "/contactus" , fragment: "champions-of-children-survey"},*/
       { name: "Teens App Survey", href: "/contactus" , fragment: "teen-app-survey"},
+      { name: "Contact Us", href: "/contactus" , fragment: "contact"},
     ]
   },
   {
