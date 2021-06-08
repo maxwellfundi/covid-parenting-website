a36fc911b3dc6138c514eceec46d154928c88564
maxwellfundi <fundimaxwell@gmail.com>
2021-06-08 05:26:25 +0300

    Adding gdp slides

---
+++ src/app/favtips/favtips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favtips',
+  templateUrl: './favtips.component.html',
+  styleUrls: ['./favtips.component.scss']
+})
+export class FavtipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
